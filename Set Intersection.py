4cf83064e802b41dbf66cb290bce776ecb47c216
notsunandandhiman <150802724+notsunandandhiman@users.noreply.github.com>
2024-11-12 21:25:56 +0530

    Add files via upload

---
+++ Set Intersection.py
@@ -0,0 +1,16 @@
+setx = {"green","blue"}
+sety = {"blue","yellow"}
+print("Orignal set elements")
+print(setx)
+print(sety)
+print("\n Intersection of two said sets:")
+setz = setx.intersection(sety)
+print(setz)
+
+print("\n.union of two said sets:")
+setz = setx.union(sety)
+print(setz)
+
+print("\n diffrenceof two said sets:")
+setz = setx.diffrence(sety)
+print(setz)
